1a9e0d286da1418aa6b6d6095e5471b76135a463
Mohamed Sadjad OUSMANE <sadjad.ousmane@mehriso.com>
2023-05-16 08:16:02 +0100

    fix: partial

---
+++ src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'prisma/prisma';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    return await this.prisma.user.create({
+      data: {
+        age: 15,
+        country: 'US',
+        email: 'user@prisma.com',
+        name: 'hello world',
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
